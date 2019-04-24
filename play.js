2f04fc46702a76342497117c66049b9f07a8be14
= <=>
2019-04-25 00:01:40 +0200

    let and const

--- play.js
+++ play.js
@@ -1,6 +1,12 @@
-var name = "Max";
-var age = 23;
-var hasHobbies = true;
+const name = "Max";
+let age = 23;
+const hasHobbies = true;
+
+console.log(`Age before change: ${age}`);
+
+age = 25;
+
+console.log(`Age after change: ${age}`);
 
 function summarizeUser(userName, userAge, userHasHobbies){
     return `Name is ${userName}, age is ${userAge} and the user has hobbies: ${userHasHobbies}`;
