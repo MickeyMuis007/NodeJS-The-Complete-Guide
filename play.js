313da1d7ea6ab7252c78fa9cdbe18284506b0242
= <=>
2019-04-25 11:45:45 +0200

    Understanding arror functions

--- play.js
+++ play.js
@@ -2,14 +2,15 @@ const name = "Max";
 let age = 23;
 const hasHobbies = true;
 
-console.log(`Age before change: ${age}`);
-
-age = 25;
-
-console.log(`Age after change: ${age}`);
-
-function summarizeUser(userName, userAge, userHasHobbies){
+const summarizeUser = (userName, userAge, userHasHobbies) => {
     return `Name is ${userName}, age is ${userAge} and the user has hobbies: ${userHasHobbies}`;
 }
 
+const add = (a, b) => a + b;
+const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+
 console.log(summarizeUser(name, age, hasHobbies));
+console.log('Add:', add(1,2));
+console.log('Add One:', addOne(1));
+console.log('Add Random', addRandom());
