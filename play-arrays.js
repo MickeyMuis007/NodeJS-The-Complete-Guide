0bd2367b6cad2712ed0c010883d1793e270cf881
= <=>
2019-04-25 13:56:51 +0200

    Understanding spread and rest operators

--- play-arrays.js
+++ play-arrays.js
@@ -2,10 +2,10 @@ const hobbies = ['Sport', 'Cooking'];
 
 console.log('For each loop: ');
 for (let hobby of hobbies) {
-    console.log(hobby);
+    console.log(`- ${hobby}`);
 }
 
-console.log('\nMap Hobbies:', hobbies.map(hobby => `Hobby: ${hobby}`));
+console.log('Map Hobbies:', hobbies.map(hobby => `Hobby: ${hobby}`));
 console.log('Original Hobbies:', hobbies);
 
 // - Objects and arrays in javascript is reference types. You get a reference to where they are stored.
@@ -14,3 +14,35 @@ console.log('Original Hobbies:', hobbies);
 
 hobbies.push('Programming')
 console.log('Additional Hobbies: ', hobbies);
+
+
+console.log('\nUnderstanding spread operator and rest operator:')
+const person = {
+    name: 'Max',
+    age: 29,
+    greet() {
+        console.log(this);
+        console.log(`Hi, I am ${this.name}`); // *this refers to the current person object scope
+    }
+}
+
+const copyArraySlice = hobbies.slice(); 
+const addArray = [hobbies];  // This create a array with hobbies array as a item
+const copyArraySpread = [...hobbies];
+const copyPerson = {...person}
+
+console.log('Copy Array using slice:', copyArraySlice);
+console.log('Add array as item:', addArray);
+console.log('Copy array using spread operator:', copyArraySpread);
+console.log('Copied person:', copyPerson);
+
+console.log('\nRest operator:');
+const toArray = (arg1, arg2, arg3) => {
+    return [arg1, arg2, arg3];
+}
+const toArrayRest = (...args) => {
+    return args;
+}
+
+console.log('To array function:', toArray(1, 2, 3));
+console.log('To array rest:', toArrayRest(1, 2, 3, 4));
