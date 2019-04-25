77d91b7f848a519b368ed8238b29e2969d05a3d8
= <=>
2019-04-25 13:10:07 +0200

    Object arrays and reference types

--- play-arrays.js
+++ play-arrays.js
@@ -5,5 +5,12 @@ for (let hobby of hobbies) {
     console.log(hobby);
 }
 
-console.log('\nMap:', hobbies.map(hobby => `Hobby: ${hobby}`));
+console.log('\nMap Hobbies:', hobbies.map(hobby => `Hobby: ${hobby}`));
 console.log('Original Hobbies:', hobbies);
+
+// - Objects and arrays in javascript is reference types. You get a reference to where they are stored.
+// - Therefore for the constant hoppies you can add new items to the object since only the reference
+// to the object is stored as a const.
+
+hobbies.push('Programming')
+console.log('Additional Hobbies: ', hobbies);
