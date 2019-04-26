ec84c4559af57f54e3710f22828deae61cc133ac
= <=>
2019-04-26 12:24:24 +0200

    Passing request bodies

--- node_server/app.js
+++ node_server/app.js
@@ -8,12 +8,25 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type"text" /><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type"text" name="message" /><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        console.log('Message')
+        req.on('data', (chunk) => {
+            console.log('data')
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
