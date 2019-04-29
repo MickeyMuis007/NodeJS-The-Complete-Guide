eb42816e0a09b5c670c738690eab5667ea788aa1
= <=>
2019-04-29 14:45:32 +0200

    Adding middleware

--- express_node_server/app.js
+++ express_node_server/app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Inside a middleware');
+    next(); // Allows request to go to the next middleware inline
+})
+
+app.use((req, res, next) => {
+    console.log('Inside another middleware');
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
