91344327e8739b95a720b3fbfcdd0eccc0b14beb
Michael Hendricks <mihendricks1@gmail.com>
2019-05-05 00:23:52 +0200

    Finishing the PUG Template

--- express_node_server/app.js
+++ express_node_server/app.js
@@ -17,6 +17,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render("page-not-found");
+    res.status(404).render("page-not-found", {pageTitle: 'Page Not Found!'});
 })
 app.listen(3000);
