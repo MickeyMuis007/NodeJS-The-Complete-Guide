2714f1f17d7f695fe628e8b67039ae10d3b9c2bf
Michael Hendricks <mihendricks1@gmail.com>
2019-05-06 14:48:09 +0200

    Adding Controllers

--- express_node_server/app.js
+++ express_node_server/app.js
@@ -10,6 +10,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render("page-not-found", {pageTitle: 'Page Not Found!', path: ''});
-});
+app.use(errorController.getPageNotFound);
 
 app.listen(3000);
