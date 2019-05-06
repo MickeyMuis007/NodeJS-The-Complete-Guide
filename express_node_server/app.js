cda0cb92e8b46cdce5532cda832c6c0e2ccedb07
Michael Hendricks <mihendricks1@gmail.com>
2019-05-06 07:11:31 +0200

    Working with handlebars

--- express_node_server/app.js
+++ express_node_server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 const path = require('path');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
