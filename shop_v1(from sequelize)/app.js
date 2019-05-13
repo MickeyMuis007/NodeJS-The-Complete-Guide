f37c20c2b72a49e1627a95d78ec30b88ebba863c
Michael Hendricks <mihendricks1@gmail.com>
2019-05-13 11:18:25 +0200

    One-to-Many and Many-to-Many relationships

--- shop_v1(from sequelize)/app.js
+++ shop_v1(from sequelize)/app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -35,11 +37,17 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+
 
 // Sync your models you define to the database and create tables etc based on the models definition you specified.
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then(result => {
     return User.findByPk(1);
   })
