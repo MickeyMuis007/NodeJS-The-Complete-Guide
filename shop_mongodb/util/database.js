b17aebb96ab0845ecccb217402fe6451d2deadcd
Michael Hendricks <mihendricks1@gmail.com>
2019-05-14 11:06:04 +0200

    Fetching the database connection

--- shop_mongodb/util/database.js
+++ shop_mongodb/util/database.js
@@ -1,13 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://michaelh:Jordan01@cluster0-kdwgq.mongodb.net/test?retryWrites=true')
+  MongoClient.connect('mongodb+srv://michaelh:Jordan01@cluster0-kdwgq.mongodb.net/shop?retryWrites=true')
   .then(client => {
     console.log("Connected!");
-    callback(client);
+    _db = client.db();
+    callback();
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
