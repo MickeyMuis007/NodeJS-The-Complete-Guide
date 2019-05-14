a3c0ba16fb54ab9b340662c27e09cfd02d17135e
Michael Hendricks <mihendricks1@gmail.com>
2019-05-14 16:04:43 +0200

    Adding the add to Cart Functionality

--- shop_mongodb/app.js
+++ shop_mongodb/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5cdaabea3b92523f72302992')
     .then(user => {
-      req.user = user
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
