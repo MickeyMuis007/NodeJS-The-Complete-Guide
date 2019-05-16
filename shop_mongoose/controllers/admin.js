ef943a38dc3a66501f6d1476f55f93dc1c493088
Michael Hendricks <mihendricks1@gmail.com>
2019-05-16 07:57:00 +0200

    Saving Data through Mongoose

--- shop_mongoose/controllers/admin.js
+++ shop_mongoose/controllers/admin.js
@@ -16,7 +16,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, "", req.user._id);
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl
+  });
   product.save()
     .then(result => {
       console.log("Created Product");
