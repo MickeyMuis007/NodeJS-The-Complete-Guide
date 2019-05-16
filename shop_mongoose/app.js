ad318e9a550b82b34474de28572774086ad6d579
Michael Hendricks <mihendricks1@gmail.com>
2019-05-16 07:30:27 +0200

    Connecting to the MongoDB Server with Mongoose

--- shop_mongoose/app.js
+++ shop_mongoose/app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -18,20 +18,22 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5cdaabea3b92523f72302992')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-})
+// app.use((req, res, next) => {
+//   User.findById('5cdaabea3b92523f72302992')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client => {
+mongoose.connect('mongodb+srv://michaelh:Jordan01@cluster0-kdwgq.mongodb.net/shop?retryWrites=true')
+  .then(result => {
+    console.log('Connected!');
   app.listen(3000);
-})
+}).catch(err => console.log(err));
