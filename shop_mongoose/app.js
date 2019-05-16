26df84bcb5a6aa7ffcf63b75fb94ac5c5c9bebc3
Michael Hendricks <mihendricks1@gmail.com>
2019-05-16 15:58:04 +0200

    Adding a User using the User Model

--- shop_mongoose/app.js
+++ shop_mongoose/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5cdaabea3b92523f72302992')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// })
+app.use((req, res, next) => {
+  User.findById('5cdd6c40db60e116981cccfb')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,5 +35,19 @@ app.use(errorController.get404);
 mongoose.connect('mongodb+srv://michaelh:Jordan01@cluster0-kdwgq.mongodb.net/shop?retryWrites=true')
   .then(result => {
     console.log('Connected!');
-  app.listen(3000);
-}).catch(err => console.log(err));
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'mike',
+          email: 'mike@mah',
+          cart: {
+            items: [
+    
+            ]
+          }
+        });
+        user.save();
+      }
+    })
+    app.listen(3000);
+  }).catch(err => console.log(err));
