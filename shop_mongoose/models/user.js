2278fd2179593ce6f15bfac441370d265990fc02
Michael Hendricks <mihendricks1@gmail.com>
2019-05-16 16:35:08 +0200

    Working on the Shopping Cart

--- shop_mongoose/models/user.js
+++ shop_mongoose/models/user.js
@@ -14,12 +14,35 @@ const userSchema = new Schema({
   cart: {
     items: [
       {
-        proudctId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+        productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
         quantity: { type: Number, required: true }
       }
     ]
   }
-})
+});
+
+// NOTE: This should be function() so that the this key word refer to the shema method context
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({ 
+      productId: product._id,
+      quantity: newQuantity })
+  }
+
+  const updatedCart = { items: updatedCartItems };
+  this.cart = updatedCart;
+  return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
 
