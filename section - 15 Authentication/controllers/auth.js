0fb14d3b362794932a07c79c9957f41d99e8465e
Michael Hendricks <mihendricks1@gmail.com>
2019-05-22 07:59:38 +0200

    Adding a tiny improvement

--- section - 15 Authentication/controllers/auth.js
+++ section - 15 Authentication/controllers/auth.js
@@ -34,25 +34,25 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({email: email})
+  User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
         return res.redirect('/signup');
       }
 
-      return bcrypt.hash(password, 12);
-    })
-    .then(hashedPassword => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] }
-      });
+      return bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
 
-      return user.save();
-    })
-    .then(result => {
-      res.redirect('/login');
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 
