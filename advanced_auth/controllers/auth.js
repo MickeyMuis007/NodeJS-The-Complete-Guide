85ef4cf9a400a84f7e49d245ee65fc580e978883
Michael Hendricks <mihendricks1@gmail.com>
2019-05-24 09:32:08 +0200

    Creating the Token

--- advanced_auth/controllers/auth.js
+++ advanced_auth/controllers/auth.js
@@ -120,9 +120,9 @@ exports.getReset = (req, res, next) => {
   let message = req.flash('error');
   if (message.length > 0)
     message = message[0];
-  else 
+  else
     message = null;
-    
+
   res.render('auth/reset', {
     path: '/reset',
     pageTitle: 'Reset Password',
@@ -132,32 +132,33 @@ exports.getReset = (req, res, next) => {
 
 exports.postReset = (req, res, next) => {
   crypto.randomBytes(32, (err, buffer) => {
-    if (err){
+    if (err) {
       console.log(err);
       return res.redirect('/reset');
     }
     const token = buffer.toString('hex');
-    User.findOne({email: req.body.email})
+    User.findOne({ email: req.body.email })
       .then(user => {
+        console.log('Im here', user)
         if (!user) {
-          req.flash('err', 'No account with that email found.');
+          req.flash('error', 'No account with that email found.');
           return res.redirect('/reset');
         }
         user.resetToken = token;
         user.resetTokenExpiration = Date.now() + 3600000;
-        return user.save();
-      })
-      .then(result => {
-        res.redirect('/');
-        transporter.sendMail({
-          to: req.body.email,
-          from: 'mike@mah',
-          subject: 'Password reset',
-          html: `
-            <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
-          `
-        });
+        return user.save()
+          .then(result => {
+            res.redirect('/');
+            transporter.sendMail({
+              to: req.body.email,
+              from: 'mike@mah',
+              subject: 'Password reset',
+              html: `
+              <p>You requested a password reset</p>
+              <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
+            `
+            });
+          });
       })
       .catch(err => console.log(err));
   })
